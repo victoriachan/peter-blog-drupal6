71cdfa9b3949fab4f7b03db356236bdd91044837
Victoria Chan <contact@victoriac.net>
2010-02-08 00:26:56 +0000

    Started on basic theming

---
+++ themes/skyblue/node.tpl.php
@@ -0,0 +1,120 @@
+<?php
+// $Id$
+/**
+ * @file node.tpl.php
+ *
+ * Theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: Node body or teaser depending on $teaser flag.
+ * - $picture: The authors picture of the node output from
+ *   theme_user_picture().
+ * - $date: Formatted creation date (use $created to reformat with
+ *   format_date()).
+ * - $links: Themed links like "Read more", "Add new comment", etc. output
+ *   from theme_links().
+ * - $name: Themed username of node author output from theme_user().
+ * - $node_url: Direct url of the current node.
+ * - $terms: the themed list of taxonomy term links output from theme_links().
+ * - $submitted: themed submission information output from
+ *   theme_node_submitted().
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $teaser: Flag for the teaser state.
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ */
+ //  dsm($node->links);
+  // foreach ($node->links as $key => $value) {
+  //   print $node->links[$key]['title'];
+  // }
+
+/**
+ * dsm(get_defined_vars())
+ * dsm($variables['template_files']);
+ * dsm($node);
+ * dsm($node->content);
+ * print $FIELD_NAME_rendered;
+ */
+?>
+
+<?php if ($video): ?>
+    <?php print $video; ?>
+<?php endif; ?>
+
+<?php if ($blog_date): ?>
+    <p class="post-date"><?php print $blog_date; ?></p>
+<?php endif; ?>
+  
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
+
+  <?php print $picture; ?>
+
+  <?php if (!$page): ?>
+    <h1 class="title"><?php print $title; ?></h1>
+  <?php else: ?>
+    <h2 class="title">
+      <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif; ?>
+
+  <?php if ($unpublished): ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?>
+  
+  <?php if ($tabs): ?>
+    <?php print $tabs; ?>
+  <?php endif; ?>
+  
+  <div class="node-content">
+    <?php print $content; ?>
+  </div>
+  
+  <?php if (!$page): ?>
+  <div class="comments">
+    <?php print $links; ?>
+  </div>
+  <?php endif; ?>
+  
+  <?php if ($terms): ?>
+  <?php if ($page): ?><hr class="end-node" /><?php endif; ?>
+  <div class="terms">
+    <h3><?php print t('Topics:'); ?></h3> 
+    <?php print $terms; ?>
+  </div>
+  <?php endif; ?>
+  
+  <?php 
+  /**
+   * node_bottom
+   */ 
+  if ($page && $node_bottom): ?>
+    <?php print $node_bottom; ?>
+  <?php endif; ?>
+</div> <!-- /node -->
+
+<?php if ($page): ?>
+  <?php print $links; ?>
+<?php endif; ?>
--- themes/skyblue/template.php
+++ themes/skyblue/template.php
@@ -5,10 +5,111 @@
  * template file for  subtheme
  */
  
-/**
- * Preprocess page template variables.
- */
-/*
-function skyblue_preprocess_page(&$vars){
+function phptemplate_preprocess(&$vars, $hook) {
+  if($hook == 'page') {
+    // Add a 'page-node' class if this is a node that is rendered as page
+    if (isset($vars['node']) && $vars['node']->type) {
+      $vars['body_classes'] .= ' page-node';
+    }
+    
+    //// Add Feed icon
+    //if($array_q[0] == 'geek'){
+    //  drupal_add_feed(url('geek/feed', array('absolute' => TRUE)), 'VictoriaC RSS | Geek');
+    //} elseif($array_q[0] == 'life') {
+    //  drupal_add_feed(url('life/feed', array('absolute' => TRUE)), 'VictoriaC RSS | Life');
+    //} elseif($array_q[0] == 'today') {
+    //  drupal_add_feed(url('today/feed', array('absolute' => TRUE)), 'VictoriaC RSS | Today');
+    //} elseif($array_q[0] == '漢字感じ') {
+    //  drupal_add_feed(url('漢字感じ/feed', array('absolute' => TRUE)), 'VictoriaC RSS | 漢字感じ');
+    //} else {
+    //  drupal_add_feed(url('feed', array('absolute' => TRUE)), 'VictoriaC RSS | All');
+    //}
+    //$vars['head'] = drupal_get_html_head();   // Refresh $head variable
+    //$vars['feed_icons'] = drupal_get_feeds();  // Refresh $feed_icons variable
+
+  }
+  
+  // Replace funny chinese characters in section name
+  //$vars['body_classes'] = str_replace('-e6-bc-a2-e5-ad-97-e6-84-9f-e3-81-98', 'kanjikanji', $vars['body_classes']);
 }
+
+function phptemplate_preprocess_page(&$vars) {
+  
+  // Add css for node pages
+  if ($vars['node']) {
+    drupal_add_css(path_to_theme() . '/css/node.css', 'theme');
+    drupal_add_css(path_to_theme() . '/css/comments.css', 'theme');
+  }
+  
+  // Add css for specific node types
+  if ($vars['node']->type == 'blog') {
+    drupal_add_css(path_to_theme() . '/css/node_blog.css', 'theme');
+  } elseif ($vars['node']->type == 'update') {
+    drupal_add_css(path_to_theme() . '/css/node_update.css', 'theme');
+  } elseif ($vars['node']->type == 'project') {
+    drupal_add_css(path_to_theme() . '/css/node_project.css', 'theme');
+  } elseif ($vars['node']->type == 'tiantian') {
+    drupal_add_css(path_to_theme() . '/css/node_tiantian.css', 'theme');
+  }
+  
+  // Add css for search
+  $array_q = explode('/', $_GET['q']);
+  if ($array_q[0] == 'search') {
+    drupal_add_css(path_to_theme() . '/css/search.css', 'theme');
+    $vars['tabs'] = null; // don't show search tabs
+  }
+  
+  //Reload css/js. This is needed for preprocess_page since css/js is already loaded.
+  $vars['styles'] = drupal_get_css();
+}
+
+function phptemplate_preprocess_node(&$vars) {
+  
+  // To access regions in nodes
+  $vars['node_bottom'] = theme('blocks', 'node_bottom');
+  
+  // Load node type-specific preprocess functions (if they exist)
+  $function = 'phptemplate_preprocess_node'.'_'. $vars['node']->type;
+  if (function_exists($function)) {
+    $function(&$vars);
+  } else {
+    // Load the usual node stuff
+    phptemplate_preprocess_node_default($vars);
+  }
+}
+
+function phptemplate_preprocess_node_default(&$vars) {
+  /**
+   * load usual node stuff
+   */
+  drupal_add_css(path_to_theme() . '/css/node.css', 'theme'); 
+   
+  // Format nice blog dates
+  $vars['blog_date'] = format_date($vars['node']->created, 'large');
+
+   // embedded video
+   if ($vars['page'] && $vars['node']->field_video[0]['value']) {
+     $vars['video'] = views_embed_view('node_content','block_1', $vars['node']->nid);
+   } 
+}
+
+/**
+* function to overwrite links. removes the reply link per node type
+*
+* @param $links
+* @param $attributes
+* @return unknown_type
 */
+// Can't name as phptemplate_links() as mothership is using that
+//function phptemplate_links($links, $attributes = array('class' => 'links')) {
+//  
+//  // Link 'Add a comment' link to node page instead of comments reply page
+//  if($links['comment_add']['href']){
+//    $arr_linkparts = explode('/', $links['comment_add']['href']);
+//    $links['comment_add']['href'] = 'node/'.$arr_linkparts[2];
+//  }
+//  // Don't show 'reply' link for comments
+//  unset($links['comment_reply']);
+//  
+//  return theme_links($links, $attributes);
+//}
