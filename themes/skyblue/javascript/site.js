7ec14756792d135eefb85aa87a32ac3eb8781625
Victoria Chan <contact@victoriac.net>
2010-02-09 22:35:14 +0000

    Added top nav rollover

--- themes/skyblue/javascript/site.js
+++ themes/skyblue/javascript/site.js
@@ -1,1 +1,12 @@
 /* Sitewide JS */
+
+/* Top bar nav drop down */
+$(document).ready(function() {
+  
+  $('#top-bar .expanded').hover( function(){
+    $('#top-bar .expanded ul.menu').show();
+  },function(){
+    $('#top-bar .expanded ul.menu').hide();
+  });
+  
+});
